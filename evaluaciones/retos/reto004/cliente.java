043acc7ea3bbe67dca81da73a8c956cce29d81c7
Silviamontes43 <silvia.montes@alumnos.uneatlantico.es>
2026-03-17 13:59:11 +0100

    reto-004

---
+++ evaluaciones/retos/reto004/cliente.java
@@ -0,0 +1,8 @@
+package vistas.fraccioN;
+public class Cliente {
+    public static void main (String [] args ) {
+        Fraccion tresQuintos= new Fraccion (3,5);
+        Fraccion dosCuartos =new Fraccion (2,4);
+       
+    }
+}
