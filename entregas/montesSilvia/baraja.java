c9a05b356f36cd1630ac5ac249c8441190f2e810
Silviamontes43 <silvia.montes@alumnos.uneatlantico.es>
2026-05-12 08:10:29 +0000

    valores y palos de la baraja

---
+++ entregas/montesSilvia/baraja.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class Baraja {
+
+    private String[] valores = {
+            "A", "2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "J", "Q", "K"
+    };
+
+    private String[] palos = {
+            "♥", "♦", "♣", "♠"
+    };
+    
